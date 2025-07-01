2b62265d2f1586696650aa8bb466c4bc5ef949b7
Infernape <albertedsonxc7355@gmail.com>
2025-07-01 20:50:08 +0530

    Create python main.py

---
+++ python main.py
@@ -0,0 +1,12 @@
+
+3. Follow the prompts to encrypt or decrypt an image.
+
+## Example
+
+- Original image: `myphoto.jpg`
+- Encrypted image: `encrypted_myphoto.jpg`
+- Decrypted image: `decrypted_myphoto.jpg`
+
+## License
+
+MIT License
